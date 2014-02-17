0123ef08ec4c11cf4406a74a028f1565f33dbfa6
Margaret Morris <margaretblue@gmail.com>
2014-02-16 22:10:27 -0800

    intsalled handlebars, bootstrap, location button

--- app.js
+++ app.js
@@ -32,8 +32,14 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+//app.get('/', routes.index);
+//app.get('/users', user.list);
+app.get('/', function(req, res) {
+  res.render('home');
+});
+app.get('/whatev', function(req, res) {
+  res.render('whatev');
+})
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
