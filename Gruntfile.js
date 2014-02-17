2203a8606952ba4241d8c159a0f987431c754a4a
Margaret Morris <margaretblue@gmail.com>
2014-02-17 00:14:41 -0800

    added gruntfile, jshint, etc

---
+++ Gruntfile.js
@@ -0,0 +1,42 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Task configuration.
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        unused: true,
+        boss: true,
+        eqnull: true,
+        browser: true,
+        globals: {}
+      },
+      all: {
+        src: ['Gruntfile.js', 'app.js', 'src/**/*.js', 'routes/**/*.js',],
+      },
+    },
+    watch: {
+      scripts: {
+        files: ['Gruntfile.js', 'app.js', 'src/**/*.js', 'routes/**/*.js'],
+        tasks: ['jshint'],
+      },
+    },
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task.
+  grunt.registerTask('default', ['jshint']);
+
+};
---
+++ README.md
@@ -0,0 +1,1 @@
+Location Check-ins with Node.js
